f4a7cb9c720dc0a3ebaab440d8adb77f05451a07
Moriel Schottlender <moriel@gmail.com>
2021-05-10 14:46:04 +0300

    Add ReplacerError component

--- src/views/Replace.vue
+++ src/views/Replace.vue
@@ -5,68 +5,7 @@
     </v-row>
     <v-row v-if="error" align="center" justify="center">
       <v-col class="col-md-8 col-sm-12">
-        <v-alert
-          border="right"
-          colored-border
-          type="error"
-          elevation="2"
-        >
-          <p class="text--primary font-weight-black">Uh oh! We could not load the requested page!</p>
-          <p>Attempted URL: <a :href="requestedUrl" target="_blank">{{requestedUrl}}</a></p>
-          <p>There could be several reasons for this:
-            <ul>
-              <li>The provided URL may be malformed or does not lead to an actual article</li>
-              <li>The target website is blocked behind a paywall, or is down</li>
-              <li>The URL requested <code>https://</code> instead of <code>http://</code></li>
-              <li>The system may be experiencing an issue</li>
-            </ul>
-          </p>
-          <p>If you're sure this URL is valid, try again in a few minutes.</p>
-          <p>If this error persists and you think there's a problem with the system, please consider <a href="" target="_blank">submitting a bug report!</a>. Please include the content of the error below (expandable).</p>
-        </v-alert>
-        <v-expansion-panels class="mb-6">
-          <v-expansion-panel>
-            <v-expansion-panel-header>Error details (click to expand)</v-expansion-panel-header>
-            <v-expansion-panel-content>
-            <v-btn
-              id="data-report-button"
-              data-clipboard-target="#error-report-content"
-              color="secondary"
-              outlined
-              @click="copyErrorToClipboard"
-            >Copy to clipboard
-            </v-btn>
-            <pre id="error-report-content" >
-  ## Request details:
-  * Timestamp: {{getCurrentUTCTime}}
-  * Requested URL: {{requestedUrl}}
-  * API URL: {{apiUrl}}
-  * Display type: {{this.$vuetify.breakpoint.smAndDown ? 'mobile' : 'desktop'}}
-
-  ## Error content:
-  ```
-  {{errorContent}}
-  ```</pre>
-            <v-btn
-              id="data-report-button"
-              data-clipboard-target="#error-report-content"
-              color="secondary"
-              outlined
-              @click="copyErrorToClipboard"
-            >Copy to clipboard
-            </v-btn>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-        </v-expansion-panels>
-
-        <v-btn
-          x-large
-          color="primary"
-          to="/"
-        >
-          <v-icon left>mdi-information</v-icon>
-          Try another website
-        </v-btn>
+        <ReplacerError :requestedUrl="requestedUrl" :apiUrl="apiUrl" :errorContent="errorContent" />
       </v-col>
     </v-row>
     <iframe
@@ -82,11 +21,11 @@
 
 <script>
 import LoadingAnimation from '../components/LoadingAnimation'
-import ClipboardJS from 'clipboard'
+import ReplacerError from '../components/ReplacerError'
 
 export default {
   name: 'Replace',
-  components: { LoadingAnimation },
+  components: { LoadingAnimation, ReplacerError },
   data () {
     return {
       error: false,
@@ -99,9 +38,6 @@ export default {
   computed: {
     requestedUrl () {
       return decodeURIComponent(this.$route.params.url)
-    },
-    getCurrentUTCTime () {
-      return new Date().toUTCString()
     }
   },
   methods: {
@@ -139,10 +75,6 @@ export default {
       }
       this.loading = false
       this.ready = true
-    },
-    copyErrorToClipboard () {
-      // eslint-disable-next-line no-unused-vars
-      const clipboard = new ClipboardJS('#data-report-button')
     }
   },
   mounted () {
